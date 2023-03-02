c41d1b8b9777fe0ace0331b0182bddfd4174ac27
Kevin <kev.martinsvieira@gmail.com>
2023-03-02 19:24:31 +0100

    feat: add navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { capitalize } from '../helpers/helpers'
+import Redirection from './shared/Redirection'
+
+type NavigationProps = {
+  to: string,
+  name: string,
+}
+
+const navigationOptions: NavigationProps[] = [
+  { to: "/", name: "pokedex" },
+  { to: "/my-team", name: "my team" },
+  { to: "/battle", name: "battle" },
+  { to: "/shop", name: "shop" },
+]
+
+export const Navigation = () => {
+  // const [hover, setHover] = useState(false)
+  return (
+    <nav className='relative bg-gray-800 z-50 py-6 text-white text-xl shadow-xl sticky top-0'>
+      <div className='w-max mx-auto flex gap-5'>
+        {navigationOptions.map((option: NavigationProps, i: number) => (
+          <Redirection
+            key={i + Date.now()}
+            to={option.to}
+            className='hover:text-yellow-500'
+            children={capitalize(option.name)}
+          />
+        ))}
+      </div>
+    </nav>
+  )
+}
+
+export default Navigation
