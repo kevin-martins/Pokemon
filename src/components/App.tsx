b27a6338527b60eac05e8170160b49719593beec
Kevin <kev.martinsvieira@gmail.com>
2023-03-02 19:21:37 +0100

    feat: set routes

---
+++ src/components/App.tsx
@@ -0,0 +1,28 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Battle from "../pages/Battle";
+import Error404 from "../pages/Error404";
+import Home from "../pages/Home";
+import PokemonInfo from "../pages/pokemon/[id]";
+import Shop from "../pages/Shop";
+import Team from "../pages/Team";
+import Navigation from "./Navigation";
+
+export const App = () => {
+  return (
+    <div className='bg-gray-800 min-h-screen'>
+      <HashRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/my-team" element={<Team />} />
+          <Route path="/battle" element={<Battle />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/pokemon/:id" element={<PokemonInfo />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
