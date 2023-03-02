d7274ded22482b7aab278489241ef67fd6b55265
Kevin <kev.martinsvieira@gmail.com>
2023-03-02 19:24:05 +0100

    feat: loading component and its style

---
+++ src/components/shared/Loading.tsx
@@ -0,0 +1,12 @@
+import Center from './Center'
+import '../../styles/loading.css'
+
+const Loading = (): JSX.Element => {
+  return (
+    <Center>
+      <div className="pokeball pulse" />
+    </Center>
+  )
+}
+
+export default Loading
