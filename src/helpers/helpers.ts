96b5bbe5685bdd36dba5db51121c6284664c2b75
Kevin <kev.martinsvieira@gmail.com>
2023-03-07 17:17:49 +0100

    fix: remove helpers, it was empty of functions

--- src/helpers/helpers.ts
+++
@@ -1,5 +0,0 @@
-import { NewPokemonEvolutionProps, NewPokemonDataProps } from "../models/pokemon"
-
-export const getPokemonIndexByName = (pokemons: NewPokemonDataProps[], name: string): number => {
-    return pokemons.findIndex((pkm: NewPokemonDataProps) => pkm.name === name)
-}
