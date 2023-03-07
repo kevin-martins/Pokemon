35ede81288cef7d1f54f30f6c730979f3fac9778
Kevin <kev.martinsvieira@gmail.com>
2023-03-07 23:42:06 +0100

    fix: cleaning the code

--- src/models/query-response/pokemon-info-by-id/sprites/version/index.ts
+++ src/models/query-response/pokemon-info-by-id/sprites/version/index.ts
@@ -7,21 +7,6 @@ import { GenerationVIProps } from './generations/generation-vi'
 import { GenerationVIIProps } from './generations/generation-vii'
 import { GenerationVIIIProps } from './generations/generation-viii'
 
-// export type GenerationProps = {
-//     back_default?: string | null
-//     back_gray?: string | null,
-//     back_female?: string | null,
-//     back_transparent?: string | null,
-//     back_shiny?: string | null
-//     back_shiny_female?: string | null,
-//     front_default?: string | null,
-//     front_gray?: string | null,
-//     front_female?: string | null,
-//     front_transparent?: string | null,
-//     front_shiny?: string | null
-//     front_shiny_female?: string | null
-// }
-
 export type VersionProps = {
     [`generation-i`]: GenerationIProps,
     [`generation-ii`]: GenerationIIProps,
