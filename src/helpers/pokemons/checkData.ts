d296794a2b52a5f0e9203baf7875c8ed225253e5
Kevin <kev.martinsvieira@gmail.com>
2023-03-07 18:05:11 +0100

    chore: mv helpers functions into cleaner files's name

--- src/helpers/pokemons/checkData.ts
+++ src/helpers/pokemons/checkData.ts
@@ -1,3 +1,35 @@
+import { NewPokemonDataProps, NewPokemonEvolutionProps } from "../../models/pokemon"
+import { getPokemonNames } from "./getData"
+
 export const isPokemonAlreadyInArray = (pokemonArray: { id: number }[], pokemonId: number): boolean => {
     return !pokemonArray.every(pokemon => pokemonId !== pokemon.id)
 }
+
+export const isEvolutionFormAlreadyInArray = (
+    evolutions: NewPokemonEvolutionProps[],
+    team: NewPokemonDataProps[]
+): boolean => {
+    const tryingToAddFormNames = getPokemonNames(evolutions)
+
+    for (const pokemon of team) {
+        const teamEvolutionFormNames = getPokemonNames(pokemon.evolutions)
+
+        for (const teamEvolutionFormName of teamEvolutionFormNames) {
+            if (tryingToAddFormNames.includes(teamEvolutionFormName)) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+// export const isEvolutionFormMaximumDiscovered = (
+//     evolutions: NewPokemonEvolutionProps[],
+//     name: string,
+// ): boolean => {
+//     const evolutionNames = getPokemonNames(evolutions)
+//     if (evolutionNames[evolutionNames.length - 1] === name) {
+//         return true
+//     }
+//     return false
+// }
--- src/helpers/pokemons/getData.ts
+++ src/helpers/pokemons/getData.ts
@@ -2,6 +2,10 @@ import { generationRange } from "../../api/select"
 import { GenerationRangeProps, NewPokemonDataProps, NewPokemonEvolutionProps } from "../../models/pokemon"
 import { EvolvesToProps } from "../../models/query-response/pokemon-evolution-chain/chain/evolves-to"
 
+export const getPokemonSpriteUrlById = (pokemonId: number | string): string => {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemonId}.png`
+}
+
 export const getEvolutionChainRecursively = (
     evolves_to: EvolvesToProps[] | [],
     evolutions: NewPokemonEvolutionProps[]
@@ -9,8 +13,8 @@ export const getEvolutionChainRecursively = (
     evolves_to.forEach((e: EvolvesToProps) => {
         evolutions.push({
             level: e.evolution_details[0].min_level,
-            to: e.species.name,
-            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${e.species.url.split('/')[6]}.png`,
+            name: e.species.name,
+            sprite: getPokemonSpriteUrlById(e.species.url.split('/')[6]),
             current: false,
         })
         return getEvolutionChainRecursively(e.evolves_to, evolutions)
@@ -40,11 +44,26 @@ export const getNextPokemonEvolutionFormData = (
     return evolutions[currentFormIndex + 1]
 }
 
-export const getPokemonDataFromEvolutions = <T extends string | number>(
+// export const getMaxEvolutionFormData = (
+//     pokemonArray: NewPokemonDataProps[],
+//     evolutions: NewPokemonEvolutionProps[]
+// ): NewPokemonDataProps => {
+//     const evolutionsFormName = getPokemonNames(evolutions)
+//     const maxEvolutionFormName = evolutionsFormName[evolutionsFormName.length - 1]
+//     return getPokemonDataByIdentifier(pokemonArray, maxEvolutionFormName)
+// }
+
+export const getPokemonNames = (
+    pokemonArray: { name: string }[]
+): string[] => {
+    return pokemonArray.map((pkm: { name: string }) => pkm.name)
+}
+
+export const getPokemonDataFormEvolutions = <T extends string | number>(
     pokedex: NewPokemonDataProps[],
     identifier: T
 ): NewPokemonDataProps[] => {
-    const evolutionFormNames = getPokemonDataByIdentifier(pokedex, identifier).evolutions.map(pkm => pkm.to)
+    const evolutionFormNames = getPokemonNames(getPokemonDataByIdentifier(pokedex, identifier).evolutions)
     return evolutionFormNames.map(name => getPokemonDataByIdentifier(pokedex, name))
 }
 
@@ -57,17 +76,36 @@ export const getPokemonDataByIdentifier = <T extends string | number>(
     return pokedex[pokedex.findIndex(pkm => pkm.id === identifier)]
 }
 
+export const getPokemonIndexByIdentifier = <T extends string | number>(
+    pokemons: NewPokemonDataProps[],
+    identifier: T
+): number => {
+    if (typeof identifier === "string")
+        return pokemons.findIndex((pkm: NewPokemonDataProps) => pkm.name === identifier)
+    return pokemons.findIndex((pkm: NewPokemonDataProps) => pkm.id === identifier)
+}
+
 export const getMissingLevelToEvolve = (
     evolutions: NewPokemonEvolutionProps[],
     currentLevel: number
 ): number => {
-    const missingLevels = getNextPokemonEvolutionFormData(evolutions).level - currentLevel
-    return missingLevels
+    return getNextPokemonEvolutionFormData(evolutions).level - currentLevel
 }
 
-export const getPokeballFilename = (name: string): string => {
-    return name.toLocaleLowerCase().split(/ ?ball/).join('ball').trim()
+export const getPokemonDiscover = (
+    evolutionNames: string[],
+    nextEvolutionName: string,
+    pokemonName: string,
+): boolean => {
+    const nextFormIndex = evolutionNames.findIndex(name => name === nextEvolutionName)
+    const currentFormIndex = evolutionNames.findIndex(name => name === pokemonName)
+
+    return currentFormIndex <= nextFormIndex
 }
+
+// export const getPokeballFilename = (name: string): string => {
+//     return name.toLocaleLowerCase().split(/ ?ball/).join('ball').trim()
+// }
 
 export const getGenerationRangeByGenerationValue = (value: string): GenerationRangeProps => {
     return generationRange.reduce((acc: GenerationRangeProps, curr: GenerationRangeProps) => {
