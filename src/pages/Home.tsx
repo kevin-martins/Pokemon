03e3c8e9b581b1f96c104143d1fe5185152ec24c
Kevin <kev.martinsvieira@gmail.com>
2023-04-25 12:31:05 +0200

    fix: remove comments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,25 +13,9 @@ export const Home = () => {
   const [generation, setGeneration] = useState<string>(generationRange.value)
   const dispatch = useAppDispatch()
 
-  // const getNextFetchCall = () => {
-  //   if (status === LoadingState.Idle && generationRange) {
-  //     const from = generationRange.from + progression
-  //     const to = from + pas
-  //     if (to >= generationRange.to) {
-  //       dispatch(fetchDataAsync({ from: from, to: generationRange.to }))
-  //       setProgression(generationRange.to)
-  //     } else {
-  //       dispatch(fetchDataAsync({ from: from, to: to }))
-  //       setProgression(to)
-  //     }
-  //   }
-  // }
-
   const getNextFetchCall = () => {
     if (status === LoadingState.Idle && generationRange.value === generation) {
       const pas = 19
-      // const from = generationRange.from + progression
-      // let to = from + pas
       const from = generationRange.from + (pas * progression)
       const to = from + pas - 1
       if (to > generationRange.to) {
@@ -40,31 +24,16 @@ export const Home = () => {
         dispatch(fetchDataAsync({ from: from, to: to }))
       }
       setProgression(current => current + 1)
-      // setProgression(current => current + pas)
       console.log(from, to, progression, generationRange)
     }
   }
 
   useEffect(() => {
     if (generationRange.value !== generation) {
-      console.log('reset')
       setProgression(0)
-      // getNextFetchCall()
       setGeneration(generationRange.value)
     }
   }, [generationRange])
-
-  // useEffect(() => {
-  //   const handleScroll = (e: any) => {
-  //     return window.scrollY
-  //   };
-
-  //   window.addEventListener('scroll', handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
 
   return (
     <div className='bg-gray-800 min-h-screen'>
@@ -72,7 +41,11 @@ export const Home = () => {
         pageStart={0}
         loadMore={getNextFetchCall}
         hasMore={progression < generationRange.to ? true : false}
-        loader={status !== LoadingState.Idle && <Loading key={progression} />}
+        loader={status !== LoadingState.Idle &&
+          <Loading
+            key={progression}
+            index={progression}
+          />}
       >
         <Pokedex />
       </InfiniteScroll>
